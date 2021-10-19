dea74d4122e42004c9a91ec2c954a83fdda821fd
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-10-19 19:21:30 +0100

    configuring dynamic routes in the gatewar service

--- gateway/src/main/java/com/elcnyazid/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/elcnyazid/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.elcnyazid.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,11 +15,23 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+	/*
 	@Bean
+
 	RouteLocator routeLocator (RouteLocatorBuilder builder){
 		return builder.routes()
-				.route((r)->r.path("/customers/**").uri("http://localhost:8081/"))
-				.route((r)->r.path("/products/**").uri("http://localhost:8082"))
+				.route((r)->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+				.route((r)->r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
 				.build();
+	}
+    */
+
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+			ReactiveDiscoveryClient rdc,
+			DiscoveryLocatorProperties properties
+	){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,properties);
 	}
 }
