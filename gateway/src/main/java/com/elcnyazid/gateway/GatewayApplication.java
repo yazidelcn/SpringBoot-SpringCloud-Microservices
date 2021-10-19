29ee4d0ef4450d15a088d6ffd68105f0f581f910
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-10-19 18:15:50 +0100

    configuring the rout whit RouteLocatorbean from a java based configuration instead of yml file

--- gateway/src/main/java/com/elcnyazid/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/elcnyazid/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.elcnyazid.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -9,5 +12,11 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
+	@Bean
+	RouteLocator routeLocator (RouteLocatorBuilder builder){
+		return builder.routes()
+				.route((r)->r.path("/customers/**").uri("http://localhost:8081/"))
+				.route((r)->r.path("/products/**").uri("http://localhost:8082"))
+				.build();
+	}
 }
