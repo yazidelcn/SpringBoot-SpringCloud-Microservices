5117ef938bf7dcfd93c5e6e1cc6968a43e95631c
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-10-21 12:16:14 +0100

    creting methods to communicate with inventory service and customers service via REST

--- billing-service/src/main/java/com/elcnyazid/billingservice/feign/CustomerRestClient.java
+++ billing-service/src/main/java/com/elcnyazid/billingservice/feign/CustomerRestClient.java
@@ -1,8 +1,13 @@
 package com.elcnyazid.billingservice.feign;
 
+import com.elcnyazid.billingservice.model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerRestClient {
+    @GetMapping(path = "/customers/{id}")
+    public Customer getCustomerById(@PathVariable Long id);
 
 }
--- billing-service/src/main/java/com/elcnyazid/billingservice/feign/ProductRestClient.java
+++ billing-service/src/main/java/com/elcnyazid/billingservice/feign/ProductRestClient.java
@@ -1,7 +1,17 @@
 package com.elcnyazid.billingservice.feign;
 
+import com.elcnyazid.billingservice.model.Product;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @FeignClient("INVENTORY-SERVICE")
 public interface ProductRestClient {
+    @GetMapping("/products")
+    PagedModel<Product> pageProducts(@RequestParam(value="page") int page, @RequestParam(value = "size") int size);
+    @GetMapping("/products/{id}")
+    Product getProductById(@PathVariable Long id);
 }
